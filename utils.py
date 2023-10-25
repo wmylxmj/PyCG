422587edf46b77863ff5b679c8a1444a27726dde
wmy <1397322329@qq.com>
2023-10-25 22:13:48 +0800

    Add files via upload

---
+++ utils.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct 25 15:03:29 2023
+
+@author: wmy
+"""
+
+import numpy as np
+
+def get_face_normals(vertices, face_vertices):
+    face_vectors = vertices[face_vertices]
+    face_normals = np.cross(face_vectors[:, 2]-face_vectors[:, 1], \
+                            face_vectors[:, 1]-face_vectors[:, 0])
+    return face_normals
+
+def cos_face_angle(face_normals, gaze_direction):
+    norm = np.linalg.norm(face_normals, axis=1) * np.linalg.norm(gaze_direction)
+    cos_angle = np.sum(face_normals*gaze_direction, axis=1) / norm
+    return cos_angle
+
+def face_visible(face_normals, gaze_direction):
+    return cos_face_angle(face_normals, gaze_direction) > 0
+    
+
+                      
